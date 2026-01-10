f29317bdd9f6a4b2900a10811a6bfb5f8e04f610
limux <liumengjiang@gmail.com>
2026-01-10 10:55:20 +0800

    add two help command step

--- hello/build.zig
+++ hello/build.zig
@@ -1,22 +1,34 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
     const exe = b.addExecutable(.{
         .name = "hello",
         .root_module = b.createModule(.{
+            .target = b.standardTargetOptions(.{}),
+            .optimize = b.standardOptimizeOption(.{}),
             .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
         }),
     });
 
+    // 一行添加 run 命令
+    const run_step = b.step("run", "运行程序");
     const run_cmd = b.addRunArtifact(exe);
-    const run_step = b.step("run", "运行编译好的程序");
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
 
     run_step.dependOn(&run_cmd.step);
+
+    // 一行添加 help 命令
+    b.step("help", "显示帮助信息").dependOn(&b.addSystemCommand(&.{
+        "echo",
+        \\用法:
+        \\  zig build run    - 运行程序
+        \\  zig build help   - 显示此帮助
+        \\  zig build        - 构建程序
+        \\  zig build install - 安装到 zig-out/bin
+    }).step);
 
     b.installArtifact(exe);
 }
