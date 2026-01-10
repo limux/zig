5f436da19776253deab99cf4a22d6de39fa8ffbc
limux <liumengjiang@gmail.com>
2026-01-10 09:13:02 +0800

    add a root module

--- hello/build.zig
+++ hello/build.zig
@@ -6,12 +6,17 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "hello",
-        //.root_module = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-        //}),
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
+
+    const run_cmd = b.addRunArtifact(exe);
+    const run_step = b.step("run", "运行编译好的程序");
+
+    run_step.dependOn(&run_cmd.step);
 
     b.installArtifact(exe);
 }
